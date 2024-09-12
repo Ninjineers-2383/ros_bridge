6f15cc366f60af800009302e660f40a5cae291eb
Henry LeCompte <henry@lunar-glass.com>
2024-09-12 13:03:55 -0400

    Fix run_sim.sh to forward x11 display to docker

--- run_sim.sh
+++ run_sim.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-
-docker run -it --rm -p 8090:8090 -p 8091:8091 -p 1735:1735 -p 5810:5810 -v $(pwd)/gradle_cache:/root/.gradle -v $(pwd):/repos/ros_bridge -e DISPLAY=192.168.1.23:0.0 ninjineers-2383/wpilib_devel /bin/bash -c "cd /repos/ros_bridge && ./gradlew simulateNative"
+xhost +
+docker run -it --rm \
+ -p 8090:8090 -p 8091:8091 -p 1735:1735 -p 5810:5810 \
+ -v $(pwd)/gradle_cache:/root/.gradle -v $(pwd):/repos/ros_bridge -v /tmp/.X11-unix:/tmp/.X11-unix \
+ -e DISPLAY=$DISPLAY ninjineers-2383/wpilib_devel \
+ /bin/bash -c "cd /repos/ros_bridge && ./gradlew simulateNative"
